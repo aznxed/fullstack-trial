fbe15836cb00e61eab8d3e4dad8b32be32e96ddb
aznxed <aznxed@ufl.edu>
2018-09-06 22:05:51 -0400

    Initial commit

---
+++ NodeJS/db.js
@@ -0,0 +1,28 @@
+/*
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/crudDB', { useNewUrlParser: true }, (err)=>{
+    if(!err){
+        console.log('MongoDB connection succeeded');
+    }
+    else{
+        console.log('Error in DB connection: ' + JSON.stringify(err, undefined, 2));
+    }
+});
+
+module.exports = mongoose;*/
+
+const { Pool, Client } = require('pg');
+
+const pool = new Pool({
+  user: 'edward',
+  host: '162.129.225.247',
+  database: 'superpacs',
+  password: 'heart1',
+  port: 5432,
+});
+
+pool.query('SELECT NOw()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+});
+
